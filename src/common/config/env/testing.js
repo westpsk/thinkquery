d741f04ad3bd436e15f63c6eb804c6af87130e8d
pansongkui <pansongkui@126.com>
2016-05-02 14:24:52 +0800

    testing.js配置

--- src/common/config/env/testing.js
+++ src/common/config/env/testing.js
@@ -1,5 +1,17 @@
 'use strict';
 
 export default {
-  
+  	port: 4350,
+  	db:{
+		type:'mysql',
+		adapter:{
+			mysql:"localhost",
+			port:"",
+			name: 'test', // database name
+			user: 'root',
+			pwd: 'ivan',
+			prefix: 'think_',
+			encoding: 'utf8'
+		}
+	}
 };
